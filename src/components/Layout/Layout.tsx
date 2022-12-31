ad831cef05ab70f85d2dab88368fc886de6c5871
Nik Tysiachnyi <nikita.tysiachnyi@gmail.com>
2022-12-31 01:08:46 +0100

    Upd navbar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,9 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 import { DocumentReportIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
 import { Link, Outlet } from "react-router-dom";
 import { ROUTES } from "../../constants/routes";
 import Cookies from "universal-cookie";
+import { useEffect, useState } from "react";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -10,108 +11,151 @@ function classNames(...classes: string[]) {
 
 function Layout() {
   const auth = new Cookies().get("TOKEN");
+
   const navigation = [
-    { name: "Home", href: ROUTES.HOME, current: false },
-    { name: "About", href: ROUTES.ABOUT, current: false },
-    { name: "Free", href: "/free", current: false },
-    { name: "Auth", href: "/auth", current: false },
+    { name: "Home", link: ROUTES.HOME, current: false, auth: false },
+    { name: "About", link: ROUTES.ABOUT, current: false, auth: false },
+    { name: "Auth", link: "/auth", current: false, auth: true },
+    { name: "Register", link: ROUTES.REGISTER, current: false, auth: false },
+    { name: "Login", link: ROUTES.LOGIN, current: false, auth: false },
   ];
-
-  !auth &&
-    navigation.push(
-      { name: "Register", href: ROUTES.REGISTER, current: false },
-      { name: "Login", href: ROUTES.LOGIN, current: false }
-    );
-
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <Disclosure as="nav" className="bg-blue-100">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
-              <div className="relative flex items-center justify-between h-16">
-                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
+      <div>
+        <nav className="bg-gray-800">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-8"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    alt="Workflow"
+                  />
                 </div>
-                <div>
-                  <div className="self-center text-lg font-semibold whitespace-nowrap text-black">
-                    <Link to={ROUTES.HOME}>
-                      <div>Open-Doc</div>
-                    </Link>
-                  </div>
-                </div>
-                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center">
-                  <div className="hidden sm:block">
-                    <div className="flex space-x-4">
-                      {navigation.map((item) => (
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    {navigation.map((item) =>
+                      item.auth && !auth ? null : (
                         <Link
                           key={item.name}
-                          to={item.href}
-                          className={classNames(
-                            item.current
-                              ? "text-emerald-400"
-                              : "hover:text-blue-700",
-                            "px-3 py-2 rounded-md text-lg font-medium"
-                          )}
-                          aria-current={item.current ? "page" : undefined}
+                          to={item.link}
+                          className={
+                            "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                          }
                         >
                           {item.name}
                         </Link>
-                      ))}
-                    </div>
+                      )
+                    )}
                   </div>
                 </div>
-                {/* <div>
-                  <Link to={ROUTES.CREATE_DOCUMENTATION}>
-                    <button
-                      type="button"
-                      className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
                     >
-                      Create Documentation
-                    </button>
-                  </Link>
-                  <span className="pl-2" />
-                  <Link to={ROUTES.PROJECTS}>
-                    <button
-                      type="button"
-                      className="bg-blue-700 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
                     >
-                      View Documentation
-                    </button>
-                  </Link>
-                </div> */}
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
             </div>
+          </div>
 
-            <Disclosure.Panel className="sm:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1">
-                {navigation.map((item) => (
-                  <Link
-                    to={item.href}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-black"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-black",
-                      "block px-3 py-2 rounded-md text-base font-medium"
-                    )}
+          <Transition
+            show={isOpen}
+            enter="transition ease-out duration-100 transform"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75 transform"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            {(ref) => (
+              <div className="md:hidden" id="mobile-menu">
+                <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <a
+                    href="#"
+                    className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                   >
-                    {item.name}
-                  </Link>
-                ))}
+                    Dashboard
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    Team
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    Projects
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    Calendar
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    Reports
+                  </a>
+                </div>
               </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 pt-2">
-        <Outlet />
+            )}
+          </Transition>
+        </nav>
+
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <Outlet />
+          </div>
+        </main>
       </div>
     </div>
   );
